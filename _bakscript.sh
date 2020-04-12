568a9061739ab1ce0adc495f3ccfd7ca7f5e2d76
Guillaume Isabelle <jgi@jgwill.com>
2020-04-12 19:45:28 -0400

    fix droxul path

--- _bakscript.sh
+++ _bakscript.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+#source ~/.bashrc
+
 export fname=$1
 export dirsrc=$2
 export droxbakroot=/Backups_py
 export localbakroot=/backups
+export droxulcmd=~/apps/node_modules/droxul/dropbox_uploader.sh
 
 echo "Executing tar czf $localbakroot/$fname.tar.gz $dirsrc ; cd $localbakroot ; droxul upload $fname.tar.gz $droxbakroot/$fname.tar.gz"
 
-tar czf $localbakroot/$fname.tar.gz $dirsrc ; cd $localbakroot ; droxul upload $fname.tar.gz $droxbakroot/$fname.tar.gz
+tar czf $localbakroot/$fname.tar.gz $dirsrc ; cd $localbakroot ; $droxulcmd upload $fname.tar.gz $droxbakroot/$fname.tar.gz
 
 echo done
 
