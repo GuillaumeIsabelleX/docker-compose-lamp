dc35741e0190ba521b07651c896c6e9123e915c1
Guillaume Isabelle <jgi@jgwill.com>
2020-05-02 00:28:14 -0400

    config: Lowered used resources

--- www/index.php
+++ www/index.php
@@ -1,6 +1,9 @@
-<h1>Hey, that is JGWill's Stack.</h1>
-<h2>What else? no your business, yet ;) </h2>
+<h1>Welcome, that is JGWill's Stack.</h1>
+<h2>An anonymous platform to help innovation. </h2>
 <hr>
+<h3>There isn't any plan for a public portal yet.  Therefore all our work and innovators we support is made anonymously.
+</h3>
 
-<script src="https://liberapay.com/jgwill/widgets/button.js"></script>
-<noscript><a href="https://liberapay.com/jgwill/donate"><img alt="Donate using Liberapay" src="https://liberapay.com/assets/widgets/donate.svg"></a></noscript>
+
+
+
