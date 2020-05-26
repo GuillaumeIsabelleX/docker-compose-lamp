31fa5ca3dc83bc26e0c56d8cf60f3ba2fdd3e2c1
Guillaume Isabelle <jgi@jgwill.com>
2020-05-26 07:29:33 -0400

    fix(sync): some var were fixed and af added

--- _drox_restore.sh
+++ _drox_restore.sh
@@ -14,6 +14,7 @@ export DBX_BAK_ROOT_PATH=/Backups_py
 export DBX_DATA_TAR_GZ=data.tar.gz
 export DBX_FILES_TAR_GZ=gi.tar.gz
 export DBX_IMG_TAR_GZ=img.tar.gz
+export DBX_AF_TAR_GZ=af.tar.gz
 export REL_TARGET_FILES_ROOT_DIR=_vh
 export REL_TARGET_FILES_DIRNAME=gi
 export REL_TARGET_IMG_DIRNAME=img
@@ -24,6 +25,8 @@ export LOCAL_WWW_ROOT_FULL_PATH=$WORKDIR/$LOCAL_WWW_ROOT_REL_PATH
 export REL_TARGET_DATA_DIR=./data/mysql
 export REL_TARGET_FILES_DIR_REL_PATH=$REL_TARGET_FILES_ROOT_DIR/$REL_TARGET_FILES_DIRNAME
 export REL_TARGET_FILES_DIR_FULL_PATH=$LOCAL_WWW_ROOT_FULL_PATH/$REL_TARGET_FILES_DIR_REL_PATH
+export VH_TARGET_FOLDER_FULL_PATH=$LOCAL_WWW_ROOT_FULL_PATH/$REL_TARGET_FILES_ROOT_DIR
+export AF_FULL_PATH=$LOCAL_WWW_ROOT_FULL_PATH/af
 export TMP_DOWNLOAD_PATH=$TEMP/_droxrestore_sync
 # Where the data is in the TAR
 export REL_SOURCE_DATA_PATH=a/docker-compose-lamp/data
@@ -43,19 +46,18 @@ mkdir -p _xtract_tmp
 
 mv $LOCAL_WWW_ROOT_REL_PATH/$REL_TARGET_FILES_DIR_REL_PATH zold
 mv $REL_TARGET_DATA_DIR zold
+mv $LOCAL_WWW_ROOT_FULL_PATH/af zold
+
+
 
 #mkdir -p $REL_TARGET_DATA_DIR 
 #mkdir -p $REL_TARGET_FILES_DIR_REL_PATH 
 
 
 droxul download $DBX_BAK_ROOT_PATH/$DBX_DATA_TAR_GZ $TMP_DOWNLOAD_PATH/$DBX_DATA_TAR_GZ
-droxul download $DBX_BAK_ROOT_PATH/$DBX_FILES_TAR_GZ $TMP_DOWNLOAD_PATH/$DBX_FILES_TAR_GZ
 droxul download $DBX_BAK_ROOT_PATH/$DBX_IMG_TAR_GZ $TMP_DOWNLOAD_PATH/$DBX_IMG_TAR_GZ
-
-#IMG - just sync
-cd $REL_TARGET_FILES_DIR_FULL_PATH && tar xzf $TMP_DOWNLOAD_PATH/$DBX_IMG_TAR_GZ img/ico
-mv img/ico .
-rmdir img
+droxul download $DBX_BAK_ROOT_PATH/$DBX_AF_TAR_GZ $TMP_DOWNLOAD_PATH/$DBX_AF_TAR_GZ
+droxul download $DBX_BAK_ROOT_PATH/$DBX_FILES_TAR_GZ $TMP_DOWNLOAD_PATH/$DBX_FILES_TAR_GZ
 
 # DATA
 cd _xtract_tmp
@@ -68,20 +70,52 @@ echo "@State : MySQL Data is Restored"
 
 cd $WORKDIR
 
+##############################################
 #files
 
 cd _xtract_tmp
-#cd $LOCAL_WWW_ROOT_REL_PATH
 
 # Extract the target dir from the archive
 tar xzf $TMP_DOWNLOAD_PATH/$DBX_FILES_TAR_GZ $REL_SOURCE_FILES_PATH/$REL_TARGET_FILES_DIR_REL_PATH
+
+#making vhost dir if not exist
+mkdir -p $VH_TARGET_FOLDER_FULL_PATH
 
 cd $REL_SOURCE_FILES_PATH
 cd $REL_TARGET_FILES_ROOT_DIR
 
 # Moving the restored DIR into the WWW full path where it belong
-mv $REL_TARGET_FILES_DIRNAME $REL_TARGET_FILES_DIR_FULL_PATH
+mv $REL_TARGET_FILES_DIRNAME $VH_TARGET_FOLDER_FULL_PATH
 echo "Files should be restored in $REL_TARGET_FILES_DIR_FULL_PATH"
+
+cd $WORKDIR
+
+
+
+##############################################
+#af
+
+cd _xtract_tmp
+
+# Extract the target dir from the archive
+tar xzf $TMP_DOWNLOAD_PATH/$DBX_AF_TAR_GZ
+
+mv www/af $LOCAL_WWW_ROOT_FULL_PATH
+echo "AF files should be restored in $LOCAL_WWW_ROOT_FULL_PATH/af"
+cd $WORKDIR
+
+
+
+
+
+##################################################
+#IMG - just sync
+#cd _xtract_tmp
+
+cd $REL_TARGET_FILES_DIR_FULL_PATH && tar xzf $TMP_DOWNLOAD_PATH/$DBX_IMG_TAR_GZ img/ico
+rm -f ico #if related link
+mv img/ico .
+rmdir img
 
 #Cleaning up
 rm $TMP_DOWNLOAD_PATH/*
