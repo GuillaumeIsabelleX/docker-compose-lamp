74f5fb741be863b455717162003514f1d3d096e1
Guillaume Isabelle <jgi@jgwill.com>
2020-11-29 09:53:32 -0500

    @STCGoal A Script that stabilise the Platform

---
+++ scripts/py-cron.sh
@@ -0,0 +1,12 @@
+#!/bin/env bash
+
+#@STCGoal A Script that stabilise the Platform
+#@STCIssue The backend crashes
+
+# restart the docker container
+docker container restart png-mysql
+##@STCIssue The container will restart anyway even if not bugged
+###@STCGoal A test script calls the SQL test table and if failedd will restart the server
+
+
+
